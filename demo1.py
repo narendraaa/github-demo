3814d94fbffad4cb3f6615a68b00cf7b40c0c9f8
naren <yoursnarendra.ln@gmail.com>
2025-01-31 08:03:03 +0530

    5th commit

--- demo1.py
+++ demo1.py
@@ -1,12 +1,12 @@
-## demo git gti demo
-##demo moidified
-git init
-git add
-git status
-git config --global user.name 'naren'
-git config --global user.email 'yoursnarendra.ln@gmail.com'
-$ git config --global --list
-user.name=naren
-user.email=yoursnarendra.ln@gmail.com
-
-git commit -m "first commit"
+# ## demo git gti demo
+# ##demo moidified
+# git init
+# git add
+# git status
+# git config --global user.name 'naren'
+# git config --global user.email 'yoursnarendra.ln@gmail.com'
+# $ git config --global --list
+# user.name=naren
+# user.email=yoursnarendra.ln@gmail.com
+#
+# git commit -m "first commit"
